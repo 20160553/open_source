5c9de98a72ac272d9199395949995aea46c19f1f
20160553 <53904156+20160553@users.noreply.github.com>
2020-10-12 23:46:34 +0900

    level_2

---
+++ dataGet.c
@@ -0,0 +1,78 @@
+#include "stems.h"
+#include <sys/time.h>
+#include <assert.h>
+#include <unistd.h>
+
+//
+// This program is intended to help you test your web server.
+// You can use it to test that you are correctly having multiple 
+// threads handling http requests.
+//
+// htmlReturn() is used if client program is a general web client
+// program like Google Chrome. textReturn() is used for a client
+// program in a embedded system.
+//
+// Standalone test:
+// # export QUERY_STRING="name=temperature&time=3003.2&value=33.0"
+// # ./dataGet.cgi
+
+void htmlReturn(void)
+{
+    char header[MAXLINE];
+    char content[MAXLINE];
+    char* buf;
+    char* ptr;
+
+    /* Make the response body */
+    sprintf(content, "%s<html>\r\n<head>\r\n", content);
+    sprintf(content, "%s<title>CGI test result</title>\r\n", content);
+    sprintf(content, "%s</head>\r\n", content);
+    sprintf(content, "%s<body>\r\n", content);
+    sprintf(content, "%s<h2>Welcome to the CGI program</h2>\r\n", content);
+    buf = Getenv("QUERY_STRING");
+    sprintf(content, "%s<p>Env : %s</p>\r\n", content, buf);
+    ptr = strsep(&buf, "&");
+    while (ptr != NULL) {
+        sprintf(content, "%s%s\r\n", content, ptr);
+        ptr = strsep(&buf, "&");
+    }
+    sprintf(content, "%s</body>\r\n</html>\r\n", content);
+
+    /* Generate the HTTP response */
+    
+    sprintf(header,"%sContent-Length: %d\r\n", header, strlen(content));
+    sprintf(header, "%sContent-Type: text/html\r\n\r\n", header);
+    
+    Write(STDOUT_FILENO, header, strlen(header));
+    Write(STDOUT_FILENO, content, strlen(content));
+    Write(STDOUT_FILENO, "\n\n", strlen("\n\n"));
+    fflush(stdout);
+}
+
+void textReturn(void)
+{
+    char content[MAXLINE];
+    char* buf;
+    char* ptr;
+
+    buf = Getenv("QUERY_STRING");
+    sprintf(content, "%sEnv : %s\n", content, buf);
+    ptr = strsep(&buf, "&");
+    while (ptr != NULL) {
+        sprintf(content, "%s%s\n", content, ptr);
+        ptr = strsep(&buf, "&");
+    }
+
+    /* Generate the HTTP response */
+    printf("Content-Length: %d\n", strlen(content));
+    printf("Content-Type: text/plain\r\n\r\n");
+    printf("%s", content);
+    fflush(stdout);
+}
+
+int main(void)
+{
+    htmlReturn();
+    //textReturn();
+    return(0);
+}
---
+++ dataPost.c
@@ -0,0 +1,25 @@
+#include "stems.h"
+#include <sys/time.h>
+#include <assert.h>
+#include <unistd.h>
+
+//
+// This program is intended to help you test your web server.
+// 
+
+int main(int argc, char* argv[])
+{
+    int bodylength = atoi(Getenv("CONTENT_LENGTH"))+1;
+    char buf[MAXBUF];
+    char response[MAXLINE];
+    Read(0, buf, bodylength);
+
+    sprintf(response,"HTTP/1.0 200 OK\r\n", response);
+    sprintf(response, "%sServer: My Web Server\r\n", response);
+    sprintf(response, "%sContent-Length: %d\r\n", response, bodylength);
+    sprintf(response, "%sContent-Type: text/plain\r\n\r\n", response);
+    sprintf(response, "%s%s\r\n", response, buf);
+    Write(STDOUT_FILENO, response, strlen(response));
+    fflush(stdout);
+    return(0);
+}
